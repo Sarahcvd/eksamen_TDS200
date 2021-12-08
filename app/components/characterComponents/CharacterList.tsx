7dbea1caba19020182a259f69958a701807cdfee
Sarah van Dijk <sarahmurstad@hotmail.com>
2021-12-08 12:58:13 +0100

    Filter working

--- app/components/characterComponents/CharacterList.tsx
+++ app/components/characterComponents/CharacterList.tsx
@@ -1,21 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { FlatList, SafeAreaView, StyleSheet, View, Text } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import rickAndMortyApi from "../../api/rickAndMortyApi";
 import CharacterListItem from "./CharacterListItem";
-import ListItemDelete from "../listComponents/ListItemDelete";
 import ListItemSeperator from "../listComponents/ListItemSeperator";
 import useApi from "../../hooks/useApi";
-import { AllCharacters } from "../../types/AllCharacters";
 import { Character } from "../../types/Character";
-import { TextInput } from "react-native-gesture-handler";
 import colors from "../../config/colors";
 import { SearchBar } from "react-native-elements";
 
-type Props = {
-  refreshList?: () => void;
-};
-
-export default function CharacterList({ refreshList }: Props) {
+export default function CharacterList() {
   const {
     data: characters,
     filteredData: filteredCharacters,
@@ -31,20 +24,6 @@ export default function CharacterList({ refreshList }: Props) {
     getAllCharacters();
   }, []);
 
-  /* const searchData = (text: string) => {
-    if (characters) {
-      setArrayHolder(characters);
-      const newData = arrayHolder.filter((item) => {
-        const itemData = item.name.toUpperCase();
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
-      });
-
-      setData(newData);
-      setText(text);
-    }
-  }; */
-
   const filterSearch: (text: string) => void = async (text: string) => {
     if (text) {
       const filteredData = await characters.filter((item: any) => {
@@ -59,35 +38,35 @@ export default function CharacterList({ refreshList }: Props) {
       setFilteredCharacters(await characters);
       setSearch(text);
     }
-
-    return (
-      <View>
-        <SearchBar
-          //style={styles.input}
-          searchIcon={{ size: 25 }}
-          onChangeText={(_text) => filterSearch(_text)}
-          onClear={() => filterSearch("")}
-          placeholder="Filter characters"
-          value={search}
-        />
-        <FlatList
-          data={filteredCharacters}
-          keyExtractor={(nameobject) => nameobject.id.toString()}
-          renderItem={({ item }) => (
-            <CharacterListItem
-              id={item.id}
-              name={item.name}
-              species={item.species}
-              image={item.image}
-            />
-          )}
-          ItemSeparatorComponent={() => <ListItemSeperator />}
-          refreshing={loading}
-          onRefresh={refreshList}
-        />
-      </View>
-    );
   };
+
+  return (
+    <View>
+      <SearchBar
+        searchIcon={{ size: 25 }}
+        onChangeText={(text: string) => filterSearch(text)}
+        onClear={() => filterSearch("")}
+        placeholder="Filter characters"
+        value={search}
+        platform={"default"}
+      />
+      <FlatList
+        data={filteredCharacters}
+        keyExtractor={(nameobject) => nameobject.id.toString()}
+        renderItem={({ item }) => (
+          <CharacterListItem
+            id={item.id}
+            name={item.name}
+            species={item.species}
+            image={item.image}
+          />
+        )}
+        ItemSeparatorComponent={() => <ListItemSeperator />}
+        refreshing={loading}
+        onRefresh={getAllCharacters}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
